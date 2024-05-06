31f5d88d39292498e194e9ee7a9f7fcbdd6d1e4e
Emeka Nnaji <emeka@Emekas-MacBook-Pro.local>
2024-05-06 19:30:40 -0400

    refactor

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -14,3 +14,23 @@ resource "proxmox_vm_qemu" "k8s-master" {
     type    = "scsi"
   }
 }
+
+resource "proxmox_virtual_environment_vm" "k8s-master" {
+  name  = "k8s-master"
+  vm_id = 100
+  clone {
+    vm_id = 7000
+  }
+  cpu {
+    cores   = 2
+    sockets = 1
+  }
+  memory {
+    dedicated = 4096
+  }
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 30
+  }
+}
