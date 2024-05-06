ef9b27996792dac9861326389e96225b07477173
Emeka Nnaji <emeka@Emekas-MacBook-Pro.local>
2024-05-06 19:32:16 -0400

    removed old resource

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -1,20 +1,3 @@
-resource "proxmox_vm_qemu" "k8s-master" {
-  name        = "k8s-master"
-  desc        = "Kubernetes Master"
-  vmid        = 100
-  target_node = "hp-server01"
-  agent       = 1
-  clone       = "ubuntu-24-cloud-init"
-  cores       = 2
-  sockets     = 1
-  memory      = 4096
-  disk {
-    size    = "30G"
-    storage = "local-lvm"
-    type    = "scsi"
-  }
-}
-
 resource "proxmox_virtual_environment_vm" "k8s-master" {
   name  = "k8s-master"
   vm_id = 100
