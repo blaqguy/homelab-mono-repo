81d00c638fe1554719ba39be2b401bd002e1440a
Emeka Nnaji <emeka@Emekas-MacBook-Pro.local>
2024-05-06 18:42:13 -0400

    format providers file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "proxmox" {
   pm_api_token_secret = "87d1f3c8-ef42-430a-905c-821dccc6b709"
-  pm_api_token_id = "terraform@pam!terraform"
-  pm_api_url = "https://192.168.1.13:8006/api2/json"
+  pm_api_token_id     = "terraform@pam!terraform"
+  pm_api_url          = "https://192.168.1.13:8006/api2/json"
 }
